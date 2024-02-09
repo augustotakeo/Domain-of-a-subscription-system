374f794c78dbb46fa84c41a25cad7335dd7dae0d
Augusto Abe <augustotakeo@gmail.com>
2024-02-08 21:36:54 -0300

    Validation of address value object

--- PaymentContext.Domain/ValueObjects/Address.cs
+++ PaymentContext.Domain/ValueObjects/Address.cs
@@ -1,3 +1,5 @@
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Shared.ValueObjects;
 
 namespace PaymentContext.Domain.ValueObjects;
@@ -12,6 +14,22 @@ public class Address : ValueObject {
         City = city;
         State = state;
         Country = country;
+
+        AddNotifications(new Contract<Notification>()
+            .Requires()
+            .IsLowerOrEqualsThan(street, 200, "Street", "Street should have no more than 200 characters")
+            .IsGreaterOrEqualsThan(street, 3, "Street", "Street should have at least 3 characters")
+            .IsLowerOrEqualsThan(neighborhood, 200, "Neighborhood", "Neighborhood should have no more than 200 characters")
+            .IsGreaterOrEqualsThan(neighborhood, 3, "Neighborhood", "Neighborhood should have more than 3 characters")
+            .AreEquals(ZipCode, 8, "Zip Code", "Zip Code should have 8 characters")
+            .IsLowerOrEqualsThan(city, 200, "City", "City should have no more than 200 characters")
+            .IsGreaterOrEqualsThan(city, 3, "City", "City should have at least 3 characters")
+            .IsLowerOrEqualsThan(state, 200, "State", "State should have no more than 200 characters")
+            .IsGreaterOrEqualsThan(state, 3, "State", "State should have at least 3 characters")
+            .IsLowerOrEqualsThan(country, 200, "Country", "Country should have no more than 200 characters")
+            .IsGreaterOrEqualsThan(country, 3, "Country", "Country should have at least 3 characters")
+            .Matches(Number, "^[0-9]", "Number")
+        );
     }
 
     public string Street { get; private set; }
