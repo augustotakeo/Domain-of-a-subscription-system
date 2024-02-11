f3ff12ecc28db8e41edc7a7a9d4239cdb5a3641e
Augusto Abe <augustotakeo@gmail.com>
2024-02-11 11:34:36 -0300

    Validation of email value object

--- PaymentContext.Domain/ValueObjects/Email.cs
+++ PaymentContext.Domain/ValueObjects/Email.cs
@@ -1,3 +1,5 @@
+using Flunt.Validations;
+using Flunt.Notifications;
 using PaymentContext.Shared.ValueObjects;
 
 namespace PaymentContext.Domain.ValueObjects;
@@ -5,6 +7,11 @@ namespace PaymentContext.Domain.ValueObjects;
 public class Email : ValueObject {
     public Email(string address) {
         Address = address;
+
+        AddNotifications(new Contract<Notification>()
+            .Requires()
+            .IsEmail(address, "Email.Address"));
     }
+    
     public string  Address { get; private set; }
 }
