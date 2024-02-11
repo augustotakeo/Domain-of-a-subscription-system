d17249855cdb462f2efdedc6b73aadf98edb74cb
Augusto Abe <augustotakeo@gmail.com>
2024-02-11 11:39:24 -0300

    Validation of name value object

--- PaymentContext.Domain/ValueObjects/Name.cs
+++ PaymentContext.Domain/ValueObjects/Name.cs
@@ -1,3 +1,5 @@
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Shared.ValueObjects;
 
 namespace PaymentContext.Domain.ValueObjects;
@@ -6,6 +8,10 @@ public class Name : ValueObject {
     public Name(string firstName, string lastName) {
         FirstName = firstName;
         LastName = lastName;
+
+        AddNotifications(new Contract<Notification>()
+            .IsBetween(FirstName.Length, 3, 100, "Name.FirstName")
+            .IsBetween(LastName.Length, 3, 200, "Name.LastName"));
     }
 
     public string FirstName { get; private set; }
