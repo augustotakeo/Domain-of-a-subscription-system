5e1a54fdead5c98f1c97a90bc3af6d3c4980ccdf
Augusto Abe <augustotakeo@gmail.com>
2024-02-11 16:28:58 -0300

    feat: Paypal transaction code update

--- PaymentContext.Domain/Entities/PaypalPayment.cs
+++ PaymentContext.Domain/Entities/PaypalPayment.cs
@@ -6,12 +6,21 @@ namespace PaymentContext.Domain.Entities;
 
 public class PaypalPayment : Payment {
     public Email Email { get; private set; }
-    public string TransactionCode { get; private set; }
+    public string? TransactionCode { get; private set; }
 
     public PaypalPayment(Email email, decimal total, decimal totalPaid, Name payer, 
         Document document) : base(total, totalPaid, payer, document) {
             Email = email;
-            
+
             AddNotifications(Email);
+    }
+
+    public void UpdateTransactionCode(string transactionCode) {
+        TransactionCode = transactionCode;
+
+        AddNotifications(new Contract<Notification>()
+            .Requires()
+            .Matches(TransactionCode, "^[0-9a-zA-Z]", "PaypalPayment.TransactionCode", "Invalid paypal transaction code")
+        );
     }
 }
