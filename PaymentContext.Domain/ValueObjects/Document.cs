1cfaf5b65cfe110a4c7c4af75f39eb0c5d238c89
Augusto Abe <augustotakeo@gmail.com>
2024-02-11 11:28:58 -0300

    Validation of document value object

--- PaymentContext.Domain/ValueObjects/Document.cs
+++ PaymentContext.Domain/ValueObjects/Document.cs
@@ -1,3 +1,5 @@
+using Flunt.Notifications;
+using Flunt.Validations;
 using PaymentContext.Domain.Enums;
 using PaymentContext.Shared.ValueObjects;
 
@@ -7,7 +9,27 @@ public class Document : ValueObject {
     public Document(string number, EDocumentType type) {
         Number = number;
         Type = type;
+
+        AddNotifications(new Contract<Notification>()
+            .Requires()
+            .IsTrue(Valid(), "Documente.Number"));
     }
     public string Number { get; private set; }
     public EDocumentType Type { get; private set; }
+
+    private bool Valid() {
+        if(string.IsNullOrEmpty(Number))
+            return false;
+
+        if(!Number.All(char.IsDigit))
+            return false;
+
+        if(Type == EDocumentType.CPF)
+            return Number.Length == 11;
+
+        if(Type == EDocumentType.CNPJ)
+            return Number.Length == 14;
+
+        return false;
+    }
 }
